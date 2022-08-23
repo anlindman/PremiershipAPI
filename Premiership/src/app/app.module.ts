0af9944e86a48e7924b0789a8ba0babc6b97a2ab
anlindman <lindman@post.com>
2022-08-23 11:38:33 +0200

    Adding component

--- Premiership/src/app/app.module.ts
+++ Premiership/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
@@ -12,7 +13,8 @@ import { PlayerFormComponent } from './player/player-form/player-form.component'
     PlayerFormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
